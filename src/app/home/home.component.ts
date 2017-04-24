3f516fc9efda34228fb6cf5cd0a1fd22bf501a6d
Detlef von der Thuesen <dthuesen@gmx.de>
2017-04-24 11:51:02 +0200

    Clean-up CSS. Change CSS to SCSS (SASS). Modify some styling in GameComponent, HomeComponent and ScoreComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-        <h1 class="center-headline">
-          <span class="trade-winds-4 textstroke">Stein, </span>
-          <span class="merida-one-4 textstroke">Schere, </span>
-          <span class="bubblegum-sans-4 textstroke">Papier.</span>
-        </h1>
-
-        <button md-raised-button class="start-button" routerLink="/game">Spiel starten!</button>
+        <md-card class="mat-card tile-no-background height-130">
+          <h1 class="center-headline">
+            <span class="trade-winds-4 textstroke">Stein, </span>
+            <span class="merida-one-4 textstroke">Schere, </span>
+            <span class="bubblegum-sans-4 textstroke">Papier.</span>
+          </h1>
+        </md-card>
+        <md-card class="mat-card tile-no-background height-130">
+          <button md-raised-button class="start-button" routerLink="/game">Spiel starten!</button>
+        </md-card>
 
   `,
   styles: []
